de7cb9ed986a4527f95ab5bf2cac51f256487161
Hong Jiang <lazyseq@gmail.com>
2023-05-24 16:15:13 +0800

    tool(emacs): set flycheck include path

---
+++ oxid/.dir-locals.el
@@ -0,0 +1,5 @@
+((nil .
+      (
+       (eval . (setq compile-command "bazel test //oxid:all"))
+       (eval . (setq flycheck-clang-include-path
+                     (list (expand-file-name "..")))))))
