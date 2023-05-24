c15a0cfcb04f1d7a964a371d698d454eb9ec5f98
Hong Jiang <lazyseq@gmail.com>
2023-05-24 15:18:12 +0800

    tool: add TypeStr for debugging

--- oxid/interpreter_util.h
+++ oxid/interpreter_util.h
@@ -1,6 +1,8 @@
 #ifndef INTERPRETER_UTIL_H_
 #define INTERPRETER_UTIL_H_
 
+#include <cstddef>
+#include <string>
 #include <variant>
 
 #include "interpreter.h"
@@ -13,6 +15,44 @@ bool holds(Interpreter::atom_value_type value) {
 }
 
 bool IsTruthy(const Interpreter::atom_value_type& v);
+
+template <typename T>
+std::string TypeStr();
+
+template <>
+inline std::string TypeStr<long>() {
+  return "long";
+}
+
+template <>
+inline std::string TypeStr<double>() {
+  return "double";
+}
+
+template <>
+inline std::string TypeStr<Expr::Symbol>() {
+  return "Expr::Symbol";
+}
+
+template <>
+inline std::string TypeStr<Interpreter::Symbol>() {
+  return "Interpreter::Symbol";
+}
+
+template <>
+inline std::string TypeStr<std::string>() {
+  return "std::string";
+}
+
+template <>
+inline std::string TypeStr<nullptr_t>() {
+  return "nullptr_t";
+}
+
+template <>
+inline std::string TypeStr<bool>() {
+  return "bool";
+}
 
 }  // namespace oxid
 
