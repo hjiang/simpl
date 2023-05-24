348911ed769edd0bb59eaee073f7d4509e2ec6a9
Hong Jiang <lazyseq@gmail.com>
2023-05-24 15:41:17 +0800

    fix: add missing config.h

---
+++ oxid/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <cstdint>
+
+namespace oxid {
+
+using int_type = int_fast64_t;
+using float_type = double;
+
+}  // namespace oxid
+
+#endif  // CONFIG_H_
