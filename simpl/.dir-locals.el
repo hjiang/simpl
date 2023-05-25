56fb78f7033a692a9118212a8ab71b3d9dacc351
Hong Jiang <lazyseq@gmail.com>
2023-05-25 13:16:42 +0800

    tool: update emacs local var

--- simpl/.dir-locals.el
+++ simpl/.dir-locals.el
@@ -1,6 +1,6 @@
 ((nil .
       (
-       (eval . (setq compile-command "bazel test //oxid:all"))
+       (eval . (setq compile-command "bazel test //simpl:all"))
        (eval . (setq flycheck-clang-include-path
                      (list (expand-file-name "..")
                            (expand-file-name "../bazel-oxid/external/gtest/googletest/include")))))))
