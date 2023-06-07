479cde2a4d498e7de30cdc5b3796a756d7fdf349
Hong Jiang <lazyseq@gmail.com>
2023-06-07 14:46:39 +0800

    test: add a few tests for logic operators

--- simpl/simpl_test.cc
+++ simpl/simpl_test.cc
@@ -39,10 +39,30 @@ TEST_F(SimplTest, InvalidToken) {
   EXPECT_THROW(run("(+ 2 3#)"), std::runtime_error);
 }
 
+TEST_F(SimplTest, AndExpectsArguments) {
+  EXPECT_THROW(run("(and)"), std::runtime_error);
+}
+
+TEST_F(SimplTest, OrExpectsArguments) {
+  EXPECT_THROW(run("(and)"), std::runtime_error);
+}
+
+TEST_F(SimplTest, OrReturnsLastFalseArgument) {
+  auto e = run("(or false nil ())");
+  EXPECT_TRUE(holds<nullptr_t>(e));
+}
+
+TEST_F(SimplTest, AndReturnsLastTrueArgument) {
+  auto e = run("(and 1 2 3)");
+  EXPECT_TRUE(holds<int_type>(e));
+  EXPECT_EQ(std::get<int_type>(e), 3);
+}
+
 TEST_F(SimplTest, Cons) {
   auto e = run("(cons 2 nil)");
   EXPECT_TRUE(holds<list_ptr_t>(e));
 }
+
 TEST_F(SimplTest, ConsEmpty) {
   auto e = run("(cons 2 '())");
   EXPECT_TRUE(holds<list_ptr_t>(e));
