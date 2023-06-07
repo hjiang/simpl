6c3d27b1bfab0b1c34d0851729efcb0ff4c702c2
Hong Jiang <lazyseq@gmail.com>
2023-06-07 14:35:56 +0800

    test: a few test for error cases

--- simpl/simpl_test.cc
+++ simpl/simpl_test.cc
@@ -4,28 +4,51 @@
 
 #include <gtest/gtest.h>
 
+#include <stdexcept>
+
 #include "simpl/ast.h"
 #include "simpl/config.h"
+#include "simpl/error.h"
 #include "simpl/interpreter_util.h"
 
 namespace simpl {
 
-TEST(Simpl, SimpleExpression) {
+class SimplTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    ClearError();
+    ClearRuntimeError();
+  }
+};
+
+TEST_F(SimplTest, SimpleExpression) {
   auto e = run("(+ 2 3)");
   EXPECT_TRUE(holds<int_type>(e));
   EXPECT_EQ(std::get<int_type>(e), 5);
 }
 
-TEST(Simpl, Cons) {
+TEST_F(SimplTest, IncompleteExpression) {
+  EXPECT_THROW(run("(+ 2 3"), std::runtime_error);
+}
+
+TEST_F(SimplTest, ExtraToken) {
+  EXPECT_THROW(run("(+ 2 3))"), std::runtime_error);
+}
+
+TEST_F(SimplTest, InvalidToken) {
+  EXPECT_THROW(run("(+ 2 3#)"), std::runtime_error);
+}
+
+TEST_F(SimplTest, Cons) {
   auto e = run("(cons 2 nil)");
   EXPECT_TRUE(holds<list_ptr_t>(e));
 }
-TEST(Simpl, ConsEmpty) {
+TEST_F(SimplTest, ConsEmpty) {
   auto e = run("(cons 2 '())");
   EXPECT_TRUE(holds<list_ptr_t>(e));
 }
 
-TEST(Simpl, ConsList) {
+TEST_F(SimplTest, ConsList) {
   auto e = run("(cons 1 '(2 3))");
   EXPECT_TRUE(holds<list_ptr_t>(e));
 }
