c1b9b1767d0882bea61cd1dd4f5db7adb60c6106
Hong Jiang <lazyseq@gmail.com>
2023-08-09 13:10:01 +0800

    refactor: simplify fn def code

--- simpl/built_in/fn.cc
+++ simpl/built_in/fn.cc
@@ -3,6 +3,7 @@
 #include "simpl/built_in/fn.h"
 
 #include <memory>
+#include <ranges>
 #include <utility>
 
 #include "simpl/ast.h"
@@ -12,19 +13,19 @@ namespace simpl {
 namespace built_in {
 
 Expr Fn::Call(Interpreter* interpreter, const ExprList& exprs) {
+  namespace rvs = std::ranges::views;
   if (exprs.size() < 1) {
     throw std::runtime_error("fn: missing arguments");
   }
   auto i = exprs.begin();
   auto params = std::get<Vector>(*i++);
-  FnDef::param_list_t param_list;
-
-  for (const auto& param : params) {
-    param_list.push_back(std::get<Symbol>(param).name);
-  }
+  auto param_names = rvs::transform(
+      params, [](const auto& param) { return std::get<Symbol>(param).name; });
   FnDef::body_t body(i, exprs.end());
-  return std::make_unique<UserFn>(FnDef(std::move(param_list), std::move(body)),
-                                  interpreter->env());
+  return std::make_unique<UserFn>(
+      FnDef(FnDef::param_list_t(param_names.begin(), param_names.end()),
+            std::move(body)),
+      interpreter->env());
 }
 
 }  // namespace built_in
