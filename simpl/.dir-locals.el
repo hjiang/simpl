0c478367b4e93db6958b33c5ff4cf8092d6f986a
Hong Jiang <lazyseq@gmail.com>
2023-08-16 00:08:40 +0800

    fix: emacs dir variable

--- simpl/.dir-locals.el
+++ simpl/.dir-locals.el
@@ -3,4 +3,4 @@
        (eval . (setq compile-command "bazel test //simpl:all"))
        (eval . (setq flycheck-clang-include-path
                      (list (expand-file-name "..")
-                           (expand-file-name "../bazel-oxid/external/gtest/googletest/include")))))))
+                           (expand-file-name "../bazel-simpl/external/gtest/googletest/include")))))))
