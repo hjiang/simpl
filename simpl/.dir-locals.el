5b811fd1d5c351b77037e2e775d25f2f70692a98
Hong Jiang <lazyseq@gmail.com>
2023-08-16 18:26:50 +0800

    chore: add local vars for cppcheck

--- simpl/.dir-locals.el
+++ simpl/.dir-locals.el
@@ -3,4 +3,7 @@
        (eval . (setq compile-command "bazel test //simpl:all"))
        (eval . (setq flycheck-clang-include-path
                      (list (expand-file-name "..")
+                           (expand-file-name "../bazel-simpl/external/gtest/googletest/include"))))
+       (eval . (setq flycheck-cppcheck-include-path
+                     (list (expand-file-name "..")
                            (expand-file-name "../bazel-simpl/external/gtest/googletest/include")))))))
