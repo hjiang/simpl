0c6fae0fef538f94dcc5fce49f689612f95a4be2
Hong Jiang <lazyseq@gmail.com>
2023-08-18 11:47:49 +0800

    refactor: use `std::transform` for function args

--- simpl/function.cc
+++ simpl/function.cc
@@ -2,16 +2,19 @@
 
 #include "simpl/function.h"
 
+#include <algorithm>
+
 #include "simpl/ast.h"
 #include "simpl/interpreter.h"
 
 namespace simpl {
 
 Expr Function::Call(Interpreter* interpreter, const ExprList& exprs) {
+  // TODO(hjiang): pass the args by pointer and avoid unnecessary copy?
   args_type args;
-  for (auto& expr : exprs) {
-    args.push_back(interpreter->Evaluate(expr));
-  }
+  std::transform(
+      exprs.cbegin(), exprs.cend(), std::back_inserter(args),
+      [interpreter](const Expr& expr) { return interpreter->Evaluate(expr); });
   return FnCall(interpreter, args);
 }
 
