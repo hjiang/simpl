7dc298513a2feba3d8ee3121464ef21d17cd9a66
Hong Jiang <hjiang@users.noreply.github.com>
2026-03-03 14:20:58 +0800

    add nix flake files

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "envel - snapshot shell env vars into an Emacs Lisp file";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
+          nativeBuildInputs = [
+	    pkgs.bazel
+            pkgs.llvmPackages.clang
+            pkgs.llvmPackages.clang-tools
+          ];
+        };
+      });
+}
