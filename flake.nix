5e4a6b7282760e379ff2598b8a646f6e81893233
Hong Jiang <hjiang@users.noreply.github.com>
2026-03-05 12:23:23 +0800

    fix test coverage reporting

--- flake.nix
+++ flake.nix
@@ -8,17 +8,27 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        gcovWrapper = pkgs.writeShellScriptBin "gcov" ''
+          exec ${pkgs.llvmPackages.llvm}/bin/llvm-cov gcov "$@"
+        '';
+      in {
         devShells.default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
           nativeBuildInputs = [
             pkgs.bazel
             pkgs.cpplint
             pkgs.expect
+            gcovWrapper
             pkgs.jdk
             pkgs.lcov
             pkgs.llvmPackages.clang
             pkgs.llvmPackages.clang-tools
+            pkgs.llvmPackages.llvm
           ];
+          shellHook = ''
+            export GCOV="${gcovWrapper}/bin/gcov"
+          '';
         };
       });
 }
